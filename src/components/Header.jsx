f7b53d7428ce58730d7cc4ec944ca018e63079f4
MartinPucan <pucan.m@seznam.cz>
2021-08-23 16:43:27 +0200

    need fix layout

---
+++ src/components/Header.jsx
@@ -0,0 +1,13 @@
+import React from 'react'
+
+function Header() {
+  return (
+    <header className="nav">
+      <div className="nav-item">About</div>
+      <div className="nav-item">Work</div>
+      <div className="nav-item">Blog</div>
+    </header>
+  )
+}
+
+export default Header
