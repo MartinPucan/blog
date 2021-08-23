20d5889a9feb342206de63299392bc80657a4831
MartinPucan <pucan.m@seznam.cz>
2021-08-23 22:24:58 +0200

    add wave to header background

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -2,10 +2,12 @@ import React from 'react'
 
 function Header() {
   return (
-    <header className="nav">
-      <div className="nav-item">About</div>
-      <div className="nav-item">Work</div>
-      <div className="nav-item">Blog</div>
+    <header className="header">
+      <nav className="nav">
+        <div className="nav-item">About</div>
+        <div className="nav-item">Work</div>
+        <div className="nav-item">Blog</div>
+      </nav>
     </header>
   )
 }
