b70591cce8f408c706ab50e4cfb43de860bd464e
MartinPucan <pucan.m@seznam.cz>
2021-10-06 14:28:35 +0200

    add some js usage

--- src/components/Javascript.jsx
+++ src/components/Javascript.jsx
@@ -1,5 +1,11 @@
 import React from 'react'
 
+const arr = [1, 5, 3, 6, 2, 9, 200, 4, 10, 40, 50];
+
+const sortArray = arr => arr.sort((a,b) => a - b)
+
+console.log(sortArray(arr))
+
 function Javascript() {
     return (
         <section id="javascript" className="wrapper">
@@ -20,6 +26,26 @@ function Javascript() {
             <h5>Basics</h5>
             <h5>Most usage</h5>
             <h5>Tips && tricks</h5>
+
+            <div>
+              <h4>Practice</h4>
+              <div className="description">
+                <p>Find first and last position of X in sorted array</p>
+                <pre>Given array: <code>['2','3','6','6','6','9','10','13','21',]</code> where <code>X</code> is 6.</pre>
+              </div>
+              <p>solution:</p>
+
+              <div className="description">
+                <p>sort method sorting elements alphabetically, for numbers we need improvements.</p>
+                <p>
+                  <code>const sortArray = arr => arr.sort((a,b) => a - b)</code>
+                </p>
+
+              </div>
+              <pre>
+
+              </pre>
+            </div>
         </section>
     )
 }
