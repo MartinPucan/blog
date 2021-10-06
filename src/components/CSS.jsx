92f5f95d77efa32c703fdf050eae67a64eee75ce
MartinPucan <pucan.m@seznam.cz>
2021-10-06 20:02:43 +0200

    add specificity content

--- src/components/CSS.jsx
+++ src/components/CSS.jsx
@@ -1,13 +1,30 @@
 import React from 'react'
 
+const codeSpecificity = `article { color: pink }
+.article { color: red }
+.article#article { color: blue }
+.article#article.title { color: purple }`
+
+
 function CSS() {
-    return (
-        <section id="css">
-            <h2>CSS</h2>
-            <h5>Specificity</h5>
-            <h5>Specificity</h5>
-        </section>
-    )
+  return (
+    <section id="css">
+      <h2>CSS</h2>
+      <h5>Specificity</h5>
+      <div className="description">
+        <p>From Highest to lowest specificity magnitude</p>
+        <ul>
+          <li>Inline style (1000)</li>
+          <li>ID (100)</li>
+          <li>Class (10)</li>
+          <li>Element (1)</li>
+        </ul>
+        <div>
+          <code> {codeSpecificity} </code>
+        </div>
+      </div>
+    </section>
+  )
 }
 
 export default CSS
