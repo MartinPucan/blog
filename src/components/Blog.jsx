5a11985899db5244915706de01df934fd09225fd
MartinPucan <pucan.m@seznam.cz>
2021-12-08 00:59:06 +0100

    created section for blog

---
+++ src/components/Blog.jsx
@@ -0,0 +1,27 @@
+import React from "react";
+
+function Blog() {
+  const blogPackage = [
+    {
+      title: `Frontend developing`,
+      content: `Please add content here!`
+    },
+    {
+      title: `AJAX calls tutorial JS`,
+      content: `Please add content here!`
+    }
+  ]
+
+  return (<section className="blog-container">
+    {blogPackage.map((article, index) => (
+      <article id={index}>
+        <a>
+          <h2>{article.title}</h2>
+          <p>{article.content}</p>
+        </a>
+      </article>
+    ))}
+  </section>);
+}
+
+export default Blog
--- src/components/Main.jsx
+++ src/components/Main.jsx
@@ -2,6 +2,7 @@ import React from 'react'
 import Javascript from "./Javascript";
 import CSS from "./CSS";
 import Table from "./Table";
+import Blog from "./Blog";
 
 function Main() {
   return (
@@ -9,6 +10,7 @@ function Main() {
       <Javascript />
       <CSS />
       <Table />
+      <Blog />
     </main>
   )
 }
