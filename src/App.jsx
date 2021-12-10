1cc2d445822120feb74df1b91f9d7dd8258532d9
MartinPucan <pucan.m@seznam.cz>
2021-12-10 21:02:13 +0100

    implemented dark-mode into app

--- src/App.jsx
+++ src/App.jsx
@@ -3,10 +3,26 @@ import Header from "./components/Header";
 import Footer from "./components/Footer";
 import Main from "./components/Main";
 import './App.scss'
+import useLocalStorage from "use-local-storage";
+import moon from './public/images/icons/moon.png'
+import sun from './public/images/icons/sun.png'
 
 function App() {
+  const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
+  const [theme, setTheme] = useLocalStorage('theme', defaultDark ? 'dark' : 'light');
+
+  const switchTheme = () => {
+    const newTheme = theme === 'light' ? 'dark' : 'light'
+    setTheme(newTheme)
+  }
+
   return (
-    <section className="container">
+    <section className="container" data-theme={theme}>
+      <div className="switch-button">
+        <button onClick={switchTheme} title="Activate dark mode">
+          {theme === 'light' ? <img src={moon} className="rotate" width={32} alt="dark-mode" /> : <img src={sun} className="rotateConversely" width={32} alt="light-mode" /> }
+        </button>
+      </div>
       <Header />
       <Main />
       <Footer />
