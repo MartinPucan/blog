700107f0447dca87a3d4f8f556454b953b9f7462
MartinPucan <pucan.m@seznam.cz>
2021-12-16 11:08:06 +0100

    still need fix router - switch

--- src/components/Javascript.jsx
+++ src/components/Javascript.jsx
@@ -28,29 +28,15 @@ function Javascript() {
                 <li>Spread operator</li>
             </ul>
             <h5>ES versions</h5>
-            <h5>ES versions</h5>
+              <div>
+                <a href="https://madasamy.medium.com/javascript-brief-history-and-ecmascript-es6-es7-es8-features-673973394df4">JS brief history and ES versions</a>
+              </div>
             <h5>Basics</h5>
             <h5>Most usage</h5>
             <h5>Tips && tricks</h5>
 
             <div>
               <h4>Practice</h4>
-              <div className="description">
-                <p>Find first and last position of X in sorted array</p>
-                <pre>Given array: <code>['2','3','6','6','6','9','10','13','21',]</code> where <code>X</code> is 6.</pre>
-              </div>
-              <p>solution:</p>
-
-              <div className="description">
-                <p>sort method sorting elements alphabetically, for numbers we need improvements.</p>
-                <p>
-                  <code>const sortArray = arr => arr.sort((a,b) => a - b)</code>
-                </p>
-
-              </div>
-              <pre>
-
-              </pre>
             </div>
         </section>
     )
