c299d6f9c2257942ca91fa032e47e7c6794427b7
MartinPucan <pucan.m@seznam.cz>
2022-01-27 13:51:50 +0100

    need fix browser path

--- src/App.jsx
+++ src/App.jsx
@@ -1,13 +1,13 @@
 import React from 'react'
 import Footer from "./components/Footer";
 import Main from "./components/Main";
-import Javascript from "./components/Javascript";
+// import Javascript from "./components/Javascript";
 import './App.scss'
 import useLocalStorage from "use-local-storage";
 import moon from './public/images/icons/moon.png'
 import sun from './public/images/icons/sun.png'
-import { BrowserRouter, Route, Switch } from 'react-router-dom';
-import CSS from "./components/CSS";
+// import { BrowserRouter, Route, Switch } from 'react-router-dom';
+// import CSS from "./components/CSS";
 
 function App() {
   const defaultDark = window.matchMedia('(prefers-color-scheme: dark)').matches;
@@ -25,18 +25,18 @@ function App() {
           {theme === 'light' ? <img src={moon} className="rotate" width={32} alt="dark-mode" /> : <img src={sun} className="rotateConversely" width={32} alt="light-mode" /> }
         </button>
       </div>
-      <BrowserRouter className="header">
-        <Switch>
-          <Route path="/">
-            <Main />
-            <CSS />
-            <Footer />
-          </Route>
-          <Route path="/javascript">
-            <Javascript />
-          </Route>
-        </Switch>
-      </BrowserRouter>
+      {/*<BrowserRouter className="header">*/}
+      {/*  <Switch>*/}
+      {/*    <Route path="/">*/}
+      {/*      <Main />*/}
+      {/*      <CSS />*/}
+      {/*      <Footer />*/}
+      {/*    </Route>*/}
+      {/*    <Route path="/javascript">*/}
+      {/*      <Javascript />*/}
+      {/*    </Route>*/}
+      {/*  </Switch>*/}
+      {/*</BrowserRouter>*/}
       <Main />
       <Footer />
     </section>
